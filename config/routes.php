ffd4826f7eaca6da2c8b8a2a64a4e6965a12a621
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2026-01-21 11:45:58 +0100

    front routes add

--- config/routes.php
+++ config/routes.php
@@ -5,15 +5,13 @@ use App\controllers\front\HomeController;
 $router = new Router();
 
 // Define your routes here
-$router->get('/', [HomeController::class, 'index']);
+$router->get('/home', [HomeController::class, 'index']);
 
-$router->get('/about', function() {
-    return "About page";
-});
 
 $router->get('/user/{id}', function($params) {
     return "User ID: " . $params['id'];
 });
+
 
 $router->get('/login', [HomeController::class, 'login']);
 $router->post('/login', [HomeController::class, 'loginSubmit']);
