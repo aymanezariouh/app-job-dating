e5d4856ada21494b8a6b5c29b4ca839ab48dbd37
ayoub elfaqir <ayoub.elfaqir1@gmail.com>
2026-01-21 20:32:45 +0100

    setup controller Ãde page student

--- app/controllers/front/HomeController.php
+++ app/controllers/front/HomeController.php
@@ -3,58 +3,30 @@
 namespace App\controllers\front;
 
 use App\core\Controller;
-use App\core\View;
+use App\core\Session;
+use App\models\Announcement;
+use App\models\Student;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return $this->render('home/index', [
-            'title' => 'Welcome to Framework Minimaliste'
-        ]);
-    }
+        $userId = 5;
 
-    public function about()
-    {
-        return $this->render('home/about', [
-            'title' => 'About Us'
-        ]);
-    }
-
-    public function login()
-    {
-        return $this->render('auth/login', []);
-    }
-
-    public function loginSubmit()
-    {
-        $email = $_POST['email'] ?? '';
-        $password = $_POST['password'] ?? '';
-        if ($this->auth->login($email, $password)) {
-            header('Location: /');
-            exit;
-        } else {
-            $errors = $this->auth->errors();
-            return $this->render('auth/login', ['errors' => $errors]);
-        };
-    }
-
-    public function register()
-    {
-        return $this->render('auth/register', ['errors' => []]);
-    }
-
-    public function registerSubmit()
-    {
-        $name = $_POST['name'] ?? '';
-        $email = $_POST['email'] ?? '';
-        $password = $_POST['password'] ?? '';
-        
-        if ($this->auth->register($name, $email, $password)) {
-            header('Location: /login');
-            exit;
+        $studentData = null;
+        if ($userId) {
+            $studentModel = new Student();
+            $studentData = $studentModel->getStudent($userId);
         }
-        
-        return $this->render('auth/register', ['errors' => $this->auth->errors()]);
+
+        $announcementModel = new Announcement();
+        $jobs = $announcementModel->allWithCompany();
+        $totalJobs = $announcementModel->countActive();
+
+        return $this->render('frontend/index.twig', [
+            'student' => $studentData,
+            'jobs' => $jobs,
+            'totalJobs' => $totalJobs
+        ]);
     }
 }
